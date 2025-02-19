7ec2a5c78961428a3a8461b242aaa724d429103f
Covac45 <covac45@live.co.uk>
2025-02-19 00:04:40 +0000

    Implemented all-pies component from static BPS site and altered to facilitate dynamic fetching of data.

--- src/app/pies/allpies/allpies.component.ts
+++ src/app/pies/allpies/allpies.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IProduct } from '../../product-details/product-model';
+import { ProductService } from '../../product-service/product.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'BPS-allpies',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './allpies.component.html',
   styleUrl: './allpies.component.scss'
 })
+
 export class AllpiesComponent {
+
+  productSvc = inject(ProductService);
+  
+  public allProducts!: IProduct[];
+
+  ngOnInit(): void{
+    this.productSvc.GetProducts().subscribe(products => {
+      this.allProducts = products,
+      products.forEach(product => console.log(JSON.stringify(product)))
+    });
+  }
 
 }
