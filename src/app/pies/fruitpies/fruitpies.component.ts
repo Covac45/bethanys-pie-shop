6299caf5a85eb020fdd6b2e477b9e183ca06772a
Covac45 <covac45@live.co.uk>
2025-02-19 22:57:47 +0000

    Added jumbotron component to fruitpies component

--- src/app/pies/fruitpies/fruitpies.component.ts
+++ src/app/pies/fruitpies/fruitpies.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductJumbotronComponent } from "../../product-jumbotron/product-jumbotron.component";
 
 @Component({
   selector: 'BPS-fruitpies',
-  imports: [RouterModule],
+  imports: [RouterModule, ProductJumbotronComponent],
   templateUrl: './fruitpies.component.html',
   styleUrl: './fruitpies.component.scss'
 })
 export class FruitpiesComponent {
+
+  public jumbotronFruitPies: string = "/assets/images/products/strawberrypie.jpg";
 
 }
