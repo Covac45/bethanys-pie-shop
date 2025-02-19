635035f1ebb52c4c841b7fadc8e5846acf57d1bf
Covac45 <covac45@live.co.uk>
2025-02-19 22:57:56 +0000

    Added jumbotron component to seasonalpies component

--- src/app/pies/seasonalpies/seasonalpies.component.ts
+++ src/app/pies/seasonalpies/seasonalpies.component.ts
@@ -3,10 +3,11 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IProduct } from '../../product-details/product-model';
 import { ProductService } from '../../product-service/product.service';
+import { ProductJumbotronComponent } from "../../product-jumbotron/product-jumbotron.component";
 
 @Component({
   selector: 'BPS-seasonalpies',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, ProductJumbotronComponent],
   templateUrl: './seasonalpies.component.html',
   styleUrl: './seasonalpies.component.scss'
 })
@@ -15,7 +16,9 @@ export class SeasonalpiesComponent {
       productSvc = inject(ProductService);
       
       public seasonalPieProducts!: IProduct[];
-    
+      public jumbotronSesaonalPies: string = "/assets/images/products/christmasapplepie.jpg";
+
+
       ngOnInit(): void{
         this.productSvc.GetProducts().subscribe(products => 
           this.seasonalPieProducts = products.filter(p => p.productCategory === "Seasonal Pies")
