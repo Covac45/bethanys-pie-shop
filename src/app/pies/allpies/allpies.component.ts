6fe3a901094283f1a24a5d532e611d553925f7cb
Covac45 <covac45@live.co.uk>
2025-02-19 22:57:25 +0000

    Added jumbotron component to allpies component

--- src/app/pies/allpies/allpies.component.ts
+++ src/app/pies/allpies/allpies.component.ts
@@ -3,10 +3,11 @@ import { IProduct } from '../../product-details/product-model';
 import { ProductService } from '../../product-service/product.service';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ProductJumbotronComponent } from "../../product-jumbotron/product-jumbotron.component";
 
 @Component({
   selector: 'BPS-allpies',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ProductJumbotronComponent],
   templateUrl: './allpies.component.html',
   styleUrl: './allpies.component.scss'
 })
@@ -16,6 +17,8 @@ export class AllpiesComponent {
   productSvc = inject(ProductService);
   
   public allProducts!: IProduct[];
+
+  public jumbotronAllPies: string = "/assets/images/carousel2.jpg";
 
   ngOnInit(): void{
     this.productSvc.GetProducts().subscribe(products => {
