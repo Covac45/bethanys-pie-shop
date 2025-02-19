ca315ff987de4f48972706e39497e5fe21f6c313
Covac45 <covac45@live.co.uk>
2025-02-19 00:06:25 +0000

    Implemented seasonalPies component from static BPS site and altered to facilitate dynamic fetching of data.

--- src/app/pies/seasonalpies/seasonalpies.component.ts
+++ src/app/pies/seasonalpies/seasonalpies.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IProduct } from '../../product-details/product-model';
+import { ProductService } from '../../product-service/product.service';
 
 @Component({
   selector: 'BPS-seasonalpies',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './seasonalpies.component.html',
   styleUrl: './seasonalpies.component.scss'
 })
 export class SeasonalpiesComponent {
+
+      productSvc = inject(ProductService);
+      
+      public seasonalPieProducts!: IProduct[];
+    
+      ngOnInit(): void{
+        this.productSvc.GetProducts().subscribe(products => 
+          this.seasonalPieProducts = products.filter(p => p.productCategory === "Seasonal Pies")
+        );
+      }
 
 }
