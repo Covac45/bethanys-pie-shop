12d3eaff53e1d04ec54c083595feb0829ec81faf
Covac45 <covac45@live.co.uk>
2025-02-19 00:05:08 +0000

    Implemented cheesecakes component from static BPS site and altered to facilitate dynamic fetching of data.

--- src/app/pies/cheesecakes/cheesecakes.component.ts
+++ src/app/pies/cheesecakes/cheesecakes.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductService } from '../../product-service/product.service';
+import { CommonModule } from '@angular/common';
+import { IProduct } from '../../product-details/product-model';
 
 @Component({
   selector: 'BPS-cheesecakes',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './cheesecakes.component.html',
   styleUrl: './cheesecakes.component.scss'
 })
 export class CheesecakesComponent {
+
+    productSvc = inject(ProductService);
+    
+    public cheesecakeProducts!: IProduct[];
+  
+    ngOnInit(): void{
+      this.productSvc.GetProducts().subscribe(products => 
+        this.cheesecakeProducts = products.filter(p => p.productCategory === "Cheesecakes")
+      );
+    }
 
 }
