8ce8b9ed8ad759048d101a32be16e7f67144b77c
Covac45 <covac45@live.co.uk>
2025-02-19 22:57:34 +0000

    Added jumbotron component to cheesecakes component

--- src/app/pies/cheesecakes/cheesecakes.component.ts
+++ src/app/pies/cheesecakes/cheesecakes.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { ProductService } from '../../product-service/product.service';
 import { CommonModule } from '@angular/common';
 import { IProduct } from '../../product-details/product-model';
+import { ProductJumbotronComponent } from "../../product-jumbotron/product-jumbotron.component";
 
 @Component({
   selector: 'BPS-cheesecakes',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ProductJumbotronComponent],
   templateUrl: './cheesecakes.component.html',
   styleUrl: './cheesecakes.component.scss'
 })
@@ -16,6 +17,8 @@ export class CheesecakesComponent {
     
     public cheesecakeProducts!: IProduct[];
   
+    public jumbotronCheesecake: string = "/assets/images/products/cheesecake.jpg";
+
     ngOnInit(): void{
       this.productSvc.GetProducts().subscribe(products => 
         this.cheesecakeProducts = products.filter(p => p.productCategory === "Cheesecakes")
