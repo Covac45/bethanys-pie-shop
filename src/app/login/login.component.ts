47885e8c7af9b52aa6ff610d14a3bb29ac2e324e
Covac45 <covac45@live.co.uk>
2025-02-24 12:55:12 +0000

    Produced login component and updated site header link to refer to it.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductJumbotronComponent } from "../product-page/product-jumbotron/product-jumbotron.component";
 
 @Component({
   selector: 'BPS-login',
-  imports: [],
+  imports: [RouterModule, ProductJumbotronComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  public jumbotronLogin = '/assets/images/carousel3.jpg'
 
 }
