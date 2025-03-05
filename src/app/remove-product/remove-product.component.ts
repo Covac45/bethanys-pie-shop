bf8f97e9e408cf84c2fddbd28e2065c1b2ac39fa
Covac45 <covac45@live.co.uk>
2025-03-05 13:24:13 +0000

    created 'remove-product' component to facilitate removal of existing products

---
+++ src/app/remove-product/remove-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'BPS-remove-product',
+  imports: [],
+  templateUrl: './remove-product.component.html',
+  styleUrl: './remove-product.component.scss'
+})
+export class RemoveProductComponent {
+
+}
