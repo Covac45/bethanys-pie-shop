d2aed3ea5dd09fc0524ea57b767a73f76988cc48
Covac45 <covac45@live.co.uk>
2025-03-05 13:23:48 +0000

    Created 'create-product' component to facilitate posting of new products

---
+++ src/app/create-product/create-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'BPS-create-product',
+  imports: [],
+  templateUrl: './create-product.component.html',
+  styleUrl: './create-product.component.scss'
+})
+export class CreateProductComponent {
+
+}
