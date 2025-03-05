89fa7daa8db2d0caf86f8ef39badbd1d49e4816a
Covac45 <covac45@live.co.uk>
2025-03-05 14:15:01 +0000

    Implemented product management component to facilitate a very simple project dashboard with buttons leading to product CRUD components.
    dashboard is currently a placeholder image.

--- src/app/product-management/product-management.component.ts
+++ src/app/product-management/product-management.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product-service/product.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IDashboardData } from './IDashboardData';
+import { OrderService } from '../order-service/order.service';
 
 @Component({
   selector: 'BPS-product-management',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './product-management.component.html',
   styleUrl: './product-management.component.scss'
 })
 export class ProductManagementComponent {
 
+  dashData: IDashboardData = IDashboardData.getInstance();
+  totalProducts: number = 0;
+  averageProductPrice: number = 0;
+
+
+  mostPopularProduct: string = '';
+
+  constructor(private productSvc: ProductService, private orderSvc: OrderService) {}
+
+  ngOnInit(): void {
+    this.productSvc.GetProducts().subscribe(products => {
+      this.dashData.dashProducts = products
+      this.totalProducts = this.dashData.dashProducts.length;})
+
+    /*this.orderSvc.getOrderHistory().subscribe(orders => {
+      this.averagePrice = data.averagePrice;
+      this.mostPopularProduct = data.mostPopularProduct;
+    });*/
+  }
 }
