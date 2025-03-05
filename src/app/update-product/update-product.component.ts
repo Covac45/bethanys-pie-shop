e6a33e44f3189aff3e316a091f8ac8eb0de79d80
Covac45 <covac45@live.co.uk>
2025-03-05 13:24:37 +0000

    Created 'update-product' component to facilitate update of existing products

---
+++ src/app/update-product/update-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'BPS-update-product',
+  imports: [],
+  templateUrl: './update-product.component.html',
+  styleUrl: './update-product.component.scss'
+})
+export class UpdateProductComponent {
+
+}
